6ba04ca8523219020a54349f8ae08ec71ab80f14
Tobiasbm <tobie93@hotmail.com>
2018-02-07 17:28:22 +0100

    vue installe
    Jeg installerede vue på serveren

--- index.php
+++ index.php
@@ -3,10 +3,19 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <script type="text/javascript" src="vue.js"></script>
   </head>
   <body>
-    <h1>
-      test
+    <h1 id="app">
+      {{msg}}
     </h1>
+    <script>
+      var app = new Vue({
+        el: '#app',
+        data: {
+          msg: "Vue virker"
+        }
+      })
+    </script>
   </body>
 </html>
