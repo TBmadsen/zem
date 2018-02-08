48b51229b97cb30fe29e0e9cb2ce75a067f55fa3
Tobiasbm <tobie93@hotmail.com>
2018-02-08 11:21:56 +0100

    updated vue

--- index.php
+++ index.php
@@ -3,17 +3,19 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="css/master.css">
     <script type="text/javascript" src="vue.js"></script>
   </head>
   <body>
+
     <h1 id="app">
-      {{msg}}
+      {{headline}}
     </h1>
     <script>
       var app = new Vue({
         el: '#app',
         data: {
-          msg: "Vue virker"
+          headline: "overskirft"
         }
       })
     </script>
